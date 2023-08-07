be2159ad350df0bd4b831f004ed46251210b8537
ducnv92 <81127705+ducnv92@users.noreply.github.com>
2023-08-07 14:06:19 +0700

    update

--- ios/Chatvt-Bridging-Header.h
+++ ios/Chatvt-Bridging-Header.h
@@ -1,3 +1,8 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
+#ifndef Chatvt_Bridging_Header_h
+#define Chatvt_Bridging_Header_h
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+#import <React/RCTViewManager.h>
+
+#endif /* Chatvt_Bridging_Header_h */
