032201ca23957a0daab5c9bd27e6b34cabbb6a1f
ducnv92 <81127705+ducnv92@users.noreply.github.com>
2023-08-09 21:37:38 +0700

    update

--- ios/Chatvt-Bridging-Header.h
+++ ios/Chatvt-Bridging-Header.h
@@ -1,8 +1,1 @@
-#ifndef Chatvt_Bridging_Header_h
-#define Chatvt_Bridging_Header_h
 
-#import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
-#import <React/RCTViewManager.h>
-
-#endif /* Chatvt_Bridging_Header_h */
