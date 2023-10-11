34392a4e865b6fe4ab7a1fb5ea40ca447d4e9540
Đức Nguyễn <dev.ducnv@gmail.com>
2023-10-11 21:38:03 +0700

    release

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 const path = require("path");
 module.exports = {
+  "env": {
+    "production": {
+      "plugins": ["transform-remove-console"]
+    }
+  },
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     ["@babel/plugin-proposal-decorators", { "legacy": true }],
