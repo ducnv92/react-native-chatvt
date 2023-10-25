a47d7fdd52f3df4025853bdefa3ff241a3370907
duc <dev.ducnv@gmail.com>
2023-10-25 22:12:56 +0700

    update example

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ import { AppRegistry, LogBox } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 import { Navigation } from "react-native-navigation";
-import CodePush from 'react-native-code-push'
+// import CodePush from 'react-native-code-push'
 import 'react-native-reanimated'
 import { NativeModules } from 'react-native';
 
@@ -19,11 +19,7 @@ import {ScanQR} from "./src/scanQR";
 Navigation.registerComponent("Login", () => Login)
 Navigation.registerComponent("Admin.ScanQR", () => ScanQR)
 
-Navigation.registerComponent("App", () => CodePush({
-  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
-  updateDialog: false,
-  installMode: CodePush.InstallMode.IMMEDIATE,
-})(App))
+Navigation.registerComponent("App", () => App)
 
 
 
