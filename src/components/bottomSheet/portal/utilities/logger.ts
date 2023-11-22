81be951ab20e45175daaaf8e70f107f176eae023
duc <ducnv@shoptool.vn>
2023-11-22 22:41:46 +0700

    update

--- src/components/bottomSheet/portal/utilities/logger.ts
+++ src/components/bottomSheet/portal/utilities/logger.ts
@@ -6,7 +6,7 @@ interface PrintOptions {
 
 type Print = (options: PrintOptions) => void;
 
-let isLoggingEnabled = false;
+// let isLoggingEnabled = false;
 
 // __DEV__ global is by default not defined in React Native Web builds
 const isDev = Boolean(typeof __DEV__ !== 'undefined' && __DEV__);
@@ -16,30 +16,29 @@ const enableLogging = () => {
     console.warn('[Portal] could not enable logging on production!');
     return;
   }
-  isLoggingEnabled = true;
+  // isLoggingEnabled = true;
 };
 
 let print: Print = () => {};
 
 if (isDev) {
-  print = ({ component, method, params }) => {
-    if (!isLoggingEnabled) {
-      return;
-    }
-    let message = '';
-
-    if (typeof params === 'object') {
-      message = Object.keys(params)
-        .map(key => `${key}:${params[key]}`)
-        .join(' ');
-    } else {
-      message = `${params ?? ''}`;
-    }
-    
-      `[Portal::${[component, method].filter(Boolean).join('::')}]`,
-      message
-    );
-  };
+  // print = ({ component, method, params }) => {
+  //   if (!isLoggingEnabled) {
+  //     return;
+  //   }
+  //   let message = '';
+  //
+  //   if (typeof params === 'object') {
+  //     message = Object.keys(params)
+  //       .map(key => `${key}:${params[key]}`)
+  //       .join(' ');
+  //   } else {
+  //     message = `${params ?? ''}`;
+  //   }
+  //
+  //     `[Portal::${[component, method].filter(Boolean).join('::')}]`, message
+  //   );
+  // };
 }
 
 Object.freeze(print);
